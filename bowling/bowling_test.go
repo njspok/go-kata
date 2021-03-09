659497d7035e0b4002b768a34ad43ab9d7dad2b9
Khamov Oleg <njspok@gmail.com>
2021-03-09 22:48:21 +0500

    refactoring tests

--- bowling/bowling_test.go
+++ bowling/bowling_test.go
@@ -1,6 +1,7 @@
 package bowling
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,154 +12,187 @@ func TestBowling(t *testing.T) {
 		game := NewBowling()
 		require.NotNil(t, game)
 	})
-	t.Run("roll", func(t *testing.T) {
-		t.Run("all frames opened", func(t *testing.T) {
-			game := NewBowling()
-			stat := NewStat()
-			require.Zero(t, game.Total())
-			require.Equal(t, stat, game.Stat())
+	t.Run("rolls", func(t *testing.T) {
+		type frames []struct {
+			number    uint
+			rolls     []uint
+			total     uint
+			statFrame Frame
+		}
 
-			// 1
-			game.Roll(1)
-			game.Roll(2)
-			require.Equal(t, uint(3), game.Total())
-			stat.SetFrame(Frame{
-				Number: 1,
-				First:  1,
-				Second: 2,
-				Score:  3,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
+		type final struct {
+			rolls     []uint
+			total     uint
+			statFrame FinalFrame
+		}
 
-			// 2
-			game.Roll(3)
-			game.Roll(2)
-			require.Equal(t, uint(8), game.Total())
-			stat.SetFrame(Frame{
-				Number: 2,
-				First:  3,
-				Second: 2,
-				Score:  8,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
+		fixtures := []struct {
+			name   string
+			frames frames
+			final  final
+		}{
+			{
+				name: "all frames opened",
+				frames: frames{
+					{
+						number: 1,
+						rolls:  []uint{1, 2},
+						total:  3,
+						statFrame: Frame{
+							Number: 1,
+							First:  1,
+							Second: 2,
+							Score:  3,
+							Status: OpenedStatus,
+						},
+					},
+					{
+						number: 2,
+						rolls:  []uint{3, 2},
+						total:  uint(8),
+						statFrame: Frame{
+							Number: 2,
+							First:  3,
+							Second: 2,
+							Score:  8,
+							Status: OpenedStatus,
+						},
+					},
+					{
+						number: 3,
+						rolls:  []uint{3, 4},
+						total:  uint(15),
+						statFrame: Frame{
+							Number: 3,
+							First:  3,
+							Second: 4,
+							Score:  15,
+							Status: OpenedStatus,
+						},
+					},
+					{
+						number: 4,
+						rolls:  []uint{5, 1},
+						total:  uint(21),
+						statFrame: Frame{
+							Number: 4,
+							First:  5,
+							Second: 1,
+							Score:  21,
+							Status: OpenedStatus,
+						},
+					},
+					{
+						number: 5,
+						rolls:  []uint{3, 4},
+						total:  uint(28),
+						statFrame: Frame{
+							Number: 5,
+							First:  3,
+							Second: 4,
+							Score:  28,
+							Status: OpenedStatus,
+						},
+					},
+					{
+						number: 6,
+						rolls:  []uint{5, 0},
+						total:  uint(33),
+						statFrame: Frame{
+							Number: 6,
+							First:  5,
+							Second: 0,
+							Score:  33,
+							Status: OpenedStatus,
+						},
+					},
+					{
+						number: 7,
+						rolls:  []uint{2, 3},
+						total:  uint(38),
+						statFrame: Frame{
+							Number: 7,
+							First:  2,
+							Second: 3,
+							Score:  38,
+							Status: OpenedStatus,
+						},
+					},
+					{
+						number: 8,
+						rolls:  []uint{4, 3},
+						total:  uint(45),
+						statFrame: Frame{
+							Number: 8,
+							First:  4,
+							Second: 3,
+							Score:  45,
+							Status: OpenedStatus,
+						},
+					},
+					{
+						number: 9,
+						rolls:  []uint{2, 2},
+						total:  uint(49),
+						statFrame: Frame{
+							Number: 9,
+							First:  2,
+							Second: 2,
+							Score:  49,
+							Status: OpenedStatus,
+						},
+					},
+				},
+				final: final{
+					rolls: []uint{7, 2},
+					total: 58,
+					statFrame: FinalFrame{
+						First:  7,
+						Second: 2,
+						Third:  0,
+						Score:  58,
+						Status: OpenedStatus,
+					},
+				},
+			},
+		}
 
-			// 3
-			game.Roll(3)
-			game.Roll(4)
-			require.Equal(t, uint(15), game.Total())
-			stat.SetFrame(Frame{
-				Number: 3,
-				First:  3,
-				Second: 4,
-				Score:  15,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
+		for _, fixture := range fixtures {
+			t.Run(fixture.name, func(t *testing.T) {
+				game := NewBowling()
+				stat := NewStat()
+				require.Zero(t, game.Total())
+				require.Equal(t, stat, game.Stat())
 
-			// 4
-			game.Roll(5)
-			game.Roll(1)
-			require.Equal(t, uint(21), game.Total())
-			stat.SetFrame(Frame{
-				Number: 4,
-				First:  5,
-				Second: 1,
-				Score:  21,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
+				for _, frame := range fixture.frames {
+					t.Run(fmt.Sprintf("frame %v", frame.number), func(t *testing.T) {
+						for _, r := range frame.rolls {
+							game.Roll(r)
+						}
+						require.Equal(t, frame.total, game.Total())
+						stat.SetFrame(frame.statFrame)
+						require.Equal(t, stat, game.Stat())
+					})
+				}
 
-			// 5
-			game.Roll(3)
-			game.Roll(4)
-			require.Equal(t, uint(28), game.Total())
-			stat.SetFrame(Frame{
-				Number: 5,
-				First:  3,
-				Second: 4,
-				Score:  28,
-				Status: OpenedStatus,
+				// final frame
+				for _, r := range fixture.final.rolls {
+					game.Roll(r)
+				}
+				require.Equal(t, fixture.final.total, game.Total())
+				require.True(t, game.IsFinished())
+				stat.SetFinalFrame(fixture.final.statFrame)
+				require.Equal(t, stat, game.Stat())
 			})
-			require.Equal(t, stat, game.Stat())
-
-			// 6
-			game.Roll(5)
-			game.Roll(0)
-			require.Equal(t, uint(33), game.Total())
-			stat.SetFrame(Frame{
-				Number: 6,
-				First:  5,
-				Second: 0,
-				Score:  33,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
-
-			// 7
-			game.Roll(2)
-			game.Roll(3)
-			require.Equal(t, uint(38), game.Total())
-			stat.SetFrame(Frame{
-				Number: 7,
-				First:  2,
-				Second: 3,
-				Score:  38,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
-
-			// 8
-			game.Roll(4)
-			game.Roll(3)
-			require.Equal(t, uint(45), game.Total())
-			stat.SetFrame(Frame{
-				Number: 8,
-				First:  4,
-				Second: 3,
-				Score:  45,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
-
-			// 9
-			game.Roll(2)
-			game.Roll(2)
-			require.Equal(t, uint(49), game.Total())
-			stat.SetFrame(Frame{
-				Number: 9,
-				First:  2,
-				Second: 2,
-				Score:  49,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
-
-			// 10
-			game.Roll(7)
-			game.Roll(2)
-			err := game.Roll(5)
-			require.ErrorIs(t, err, ErrGameFinished)
-			require.Equal(t, uint(58), game.Total())
-			require.True(t, game.IsFinished())
-			stat.SetFinalFrame(FinalFrame{
-				First:  7,
-				Second: 2,
-				Third:  0,
-				Score:  58,
-				Status: OpenedStatus,
-			})
-			require.Equal(t, stat, game.Stat())
-		})
-		t.Run("all strikes", func(t *testing.T) {
-			// todo
-		})
-		t.Run("all spares", func(t *testing.T) {
-			// todo
-		})
-		t.Run("mixed", func(t *testing.T) {
-			// todo
-		})
+		}
+		//t.Run("all strikes", func(t *testing.T) {
+		//	// todo
+		//})
+		//t.Run("all spares", func(t *testing.T) {
+		//	// todo
+		//})
+		//t.Run("mixed", func(t *testing.T) {
+		//	// todo
+		//})
 	})
 }
