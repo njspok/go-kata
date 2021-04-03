537a4caefc1b3bc4eceb14d8643deccf00bc2dde
Khamov Oleg <njspok@gmail.com>
2021-04-03 21:50:18 +0500

    change test dollar

--- money/dollar_test.go
+++ money/dollar_test.go
@@ -7,18 +7,16 @@ import (
 )
 
 func TestDollar(t *testing.T) {
-	t.Run("create", func(t *testing.T) {
+	t.Run("amount", func(t *testing.T) {
 		require.Equal(t, uint(11), NewDollar(11).Amount())
-	})
-	t.Run("times", func(t *testing.T) {
-		five := NewDollar(5)
-		product := five.Times(2)
-		require.Equal(t, uint(10), product.Amount())
-		product = five.Times(3)
-		require.Equal(t, uint(15), product.Amount())
 	})
 	t.Run("equals", func(t *testing.T) {
 		require.True(t, NewDollar(5).Equals(NewDollar(5)))
 		require.False(t, NewDollar(5).Equals(NewDollar(11)))
+	})
+	t.Run("times", func(t *testing.T) {
+		five := NewDollar(5)
+		require.True(t, NewDollar(10).Equals(five.Times(2)))
+		require.True(t, NewDollar(15).Equals(five.Times(3)))
 	})
 }
