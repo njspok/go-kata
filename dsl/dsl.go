de5f67d81f799ade549c4036a5c66cc0015e41be
Khamov Oleg <njspok@gmail.com>
2021-05-26 20:49:31 +0500

    specify dsl in separate file

--- dsl/dsl.go
+++ dsl/dsl.go
@@ -1,83 +1,7 @@
 package dsl
 
-import (
-	"fmt"
-	"reflect"
-)
-
-var (
-	Root         = &SolarNode{}
-	Current Node = Root
-)
-
-type Node interface{}
-
-type SolarNode struct {
-	Name        string
-	Description string
-	Planets     []*PlanetNode
-}
-
-func (sol *SolarNode) SetName(name string) {
-	sol.Name = name
-}
-
-func (sol *SolarNode) SetDescription(desc string) {
-	sol.Description = desc
-}
-
-func (sol *SolarNode) AddPlanet(planet *PlanetNode) {
-	sol.Planets = append(sol.Planets, planet)
-}
-
-type PlanetNode struct {
-	Name        string
-	Description string
-	Mass        uint
-	Satellites  []*SatelliteNode
-}
-
-func (pl *PlanetNode) SetName(name string) {
-	pl.Name = name
-}
-
-func (pl *PlanetNode) SetMass(mass uint) {
-	pl.Mass = mass
-}
-
-func (pl *PlanetNode) SetDescription(desc string) {
-	pl.Description = desc
-}
-
-func (pl *PlanetNode) AddSatellite(sat *SatelliteNode) {
-	pl.Satellites = append(pl.Satellites, sat)
-}
-
-type SatelliteNode struct {
-	Name        string
-	Description string
-	Mass        uint
-}
-
-func (sat *SatelliteNode) SetName(name string) {
-	sat.Name = name
-}
-
-func (sat *SatelliteNode) SetMass(mass uint) {
-	sat.Mass = mass
-}
-
-func (sat *SatelliteNode) SetDescription(desc string) {
-	sat.Description = desc
-}
-
-func Clean() {
-	Root = &SolarNode{}
-	Current = Root
-}
-
 func SolarSystem(name string, f func()) {
-	Clean()
+	clean()
 
 	switch n := Current.(type) {
 	case *SolarNode:
@@ -140,19 +64,4 @@ func Satellite(name string, f func()) {
 	p := &SatelliteNode{Name: name}
 	n.AddSatellite(p)
 	process(p, f)
-}
-
-func nodeWithoutAttributePanic(node Node, attr string) {
-	panic(fmt.Sprintf(
-		"Node %s have not attribute %s",
-		reflect.TypeOf(node).String(),
-		attr,
-	))
-}
-
-func process(node Node, f func()) {
-	prev := Current
-	Current = node
-	f()
-	Current = prev
 }
