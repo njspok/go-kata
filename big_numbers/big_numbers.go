2b0c381c29722287e0618bc86f3c8668a076f6a6
Khamov Oleg <njspok@gmail.com>
2021-10-06 21:42:37 +0500

    - specify type number - refactor sum function

--- big_numbers/big_numbers.go
+++ big_numbers/big_numbers.go
@@ -4,40 +4,76 @@ import (
 	"strconv"
 )
 
-func Sum(aug string, add string) string {
-	if len(aug) < len(add) {
+// todo mayme unit
+func NumberFromInt(v int) Number {
+	return Number(strconv.Itoa(v))
+}
+
+// todo implement
+func NumberFromString(v string) (Number, error) {
+	panic("not implemented")
+}
+
+type Number string
+
+// todo i change to uint
+func (n Number) Digit(i int) int {
+	// todo check range limit
+	// todo check conversation
+
+	if i >= n.Len() {
+		return 0
+	}
+
+	// todo remove !!!
+	if i < 0 {
+		return 0
+	}
+
+	v, _ := strconv.Atoi(string(n[i]))
+	return v
+}
+
+func (n Number) Len() int {
+	return len(n)
+}
+
+func (n Number) ToBegin(v int) Number {
+	return NumberFromInt(v) + n
+}
+
+func Sum(aug Number, add Number) Number {
+	if aug.Len() < add.Len() {
 		aug, add = add, aug
 	}
 
-	var result string
+	var result Number
 	var memo int
 
-	for i1 := len(aug) - 1; i1 >= 0; i1-- {
-		n1, _ := strconv.Atoi(string(aug[i1]))
+	for i1 := aug.Len() - 1; i1 >= 0; i1-- {
+		n1 := aug.Digit(i1)
 
-		i2 := i1 - (len(aug) - len(add))
-		n2 := 0
-		if i2 >= 0 {
-			n2, _ = strconv.Atoi(string(add[i2]))
-		}
+		i2 := i1 - (aug.Len() - add.Len())
+		n2 := add.Digit(i2)
 
 		n1 += memo
 
 		s := n1 + n2
 
 		if s > 9 {
-			result = strconv.Itoa(s-10) + result
+			result = result.ToBegin(s - 10)
 			memo = 1
 			continue
 		}
 
 		memo = 0
 
-		result = strconv.Itoa(s) + result
+		result = result.ToBegin(s)
+
 	}
 
 	if memo == 1 {
-		result = "1" + result
+		result = result.ToBegin(1)
 	}
 
 	return result
--- big_numbers/big_numbers_test.go
+++ big_numbers/big_numbers_test.go
@@ -3,12 +3,17 @@ package big_numbers
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestNumberFromInt(t *testing.T) {
+	require.Equal(t, Number("123"), NumberFromInt(123))
+	require.Equal(t, Number("0"), NumberFromInt(0))
+	require.Equal(t, Number("9"), NumberFromInt(9))
+}
 
 func Test(t *testing.T) {
 
@@ -16,15 +21,15 @@ func Test(t *testing.T) {
 	// todo strings with non number characters
 
 	t.Run("success", func(t *testing.T) {
-		require.Equal(t, "0", Sum("0", "0"))
-		require.Equal(t, "4", Sum("2", "2"))
-		require.Equal(t, "2", Sum("1", "1"))
-		require.Equal(t, "10", Sum("1", "9"))
-		require.Equal(t, "18", Sum("9", "9"))
-		require.Equal(t, "19", Sum("10", "9"))
-		require.Equal(t, "120", Sum("111", "9"))
-		require.Equal(t, "353646", Sum("121323", "232323"))
-		require.Equal(t, "91002328220491911630239667963", Sum("63829983432984289347293874", "90938498237058927340892374089"))
+		require.Equal(t, Number("0"), Sum("0", "0"))
+		require.Equal(t, Number("2"), Sum("1", "1"))
+		require.Equal(t, Number("4"), Sum("2", "2"))
+		require.Equal(t, Number("10"), Sum("1", "9"))
+		require.Equal(t, Number("18"), Sum("9", "9"))
+		require.Equal(t, Number("19"), Sum("10", "9"))
+		require.Equal(t, Number("120"), Sum("111", "9"))
+		require.Equal(t, Number("353646"), Sum("121323", "232323"))
+		require.Equal(t, Number("91002328220491911630239667963"), Sum("63829983432984289347293874", "90938498237058927340892374089"))
 	})
 	t.Run("replace", func(t *testing.T) {
 		rand.Seed(time.Now().Unix())
@@ -33,8 +38,8 @@ func Test(t *testing.T) {
 
 		for i := 0; i < 1000000; i++ {
 			require.Equalf(t,
-				strconv.Itoa(aug+add),
-				Sum(strconv.Itoa(aug), strconv.Itoa(add)),
+				NumberFromInt(aug+add),
+				Sum(NumberFromInt(aug), NumberFromInt(add)),
 				fmt.Sprintf("%v,%v", aug, add),
 			)
 		}
