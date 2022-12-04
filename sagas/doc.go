65c1fea89edc2978a0bc86c727934bfd7f66c594
Khamov Oleg <njspok@gmail.com>
2022-12-04 19:59:08 +0500

    sagas questons

--- sagas/doc.go
+++ sagas/doc.go
@@ -6,3 +6,8 @@ package sagas
 // loyalty
 // delivery
 // mailing
+
+// TODO sagas questions:
+// - test for saga and test for order saga how separate?
+// - service use saga interface for best testing?
+// - sagas repository save and load aggregate
