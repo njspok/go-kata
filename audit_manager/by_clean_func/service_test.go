2c48f37d3ee95c418dedf6347db9044f8f14511f
Khamov Oleg <njspok@gmail.com>
2023-01-24 10:51:40 +0500

    audit manager

--- audit_manager/by_clean_func/service_test.go
+++ audit_manager/by_clean_func/service_test.go
@@ -1,6 +1,7 @@
 package mock_version
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -30,5 +31,77 @@ func TestAuditService(t *testing.T) {
 		srv := NewAuditService(rep, manager)
 		err := srv.AddRecord("ivan", now, "in")
 		require.NoError(t, err)
+	})
+	t.Run("fail get last record", func(t *testing.T) {
+		now := time.Now()
+
+		rep := &MockAuditRep{}
+		rep.EXPECT().
+			LastRecordByName("ivan").
+			Return(nil, errors.New("shit happens")).
+			Once()
+
+		manager := NewAuditManager()
+
+		srv := NewAuditService(rep, manager)
+		err := srv.AddRecord("ivan", now, "in")
+		require.EqualError(t, err, "shit happens")
+	})
+	t.Run("invalid direction", func(t *testing.T) {
+		now := time.Now()
+
+		rep := &MockAuditRep{}
+		rep.EXPECT().
+			LastRecordByName("ivan").
+			Return(nil, nil).
+			Once()
+
+		manager := NewAuditManager()
+
+		srv := NewAuditService(rep, manager)
+		err := srv.AddRecord("ivan", now, "incoming")
+		require.EqualError(t, err, "unknown direction")
+	})
+	t.Run("cant append record", func(t *testing.T) {
+		now := time.Now()
+
+		rep := &MockAuditRep{}
+		rep.EXPECT().
+			LastRecordByName("ivan").
+			Return(&AuditRecord{
+				Direction: In,
+				Time:      now.Add(-time.Hour),
+				Name:      "ivan",
+			}, nil).
+			Once()
+
+		manager := NewAuditManager()
+
+		srv := NewAuditService(rep, manager)
+		err := srv.AddRecord("ivan", now, "in")
+		require.EqualError(t, err, "invalid direction")
+	})
+	t.Run("fail when add record in rep", func(t *testing.T) {
+		now := time.Now()
+
+		rep := &MockAuditRep{}
+		rep.EXPECT().
+			LastRecordByName("ivan").
+			Return(nil, nil).
+			Once()
+		rep.EXPECT().
+			Add(&AuditRecord{
+				Direction: In,
+				Time:      now,
+				Name:      "ivan",
+			}).
+			Return(errors.New("shit happens")).
+			Once()
+
+		manager := NewAuditManager()
+
+		srv := NewAuditService(rep, manager)
+		err := srv.AddRecord("ivan", now, "in")
+		require.EqualError(t, err, "shit happens")
 	})
 }
