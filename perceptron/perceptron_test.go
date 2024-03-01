5556e1c5291e5aad75a9beb4f79103aa994506e5
Khamov Oleg <njspok@gmail.com>
2024-03-01 09:44:20 +0500

    perceptron

--- perceptron/perceptron_test.go
+++ perceptron/perceptron_test.go
@@ -1,21 +1,65 @@
 package perceptron
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+type SampleData struct {
+	input  []float64
+	result float64
+}
+
 func Test(t *testing.T) {
-	p := New()
+	t.Run("teacher", func(t *testing.T) {
+		// login or element simulation
+		data := []SampleData{
+			{
+				input:  []float64{0, 0},
+				result: 0,
+			},
+			{
+				input:  []float64{0, 1},
+				result: 1,
+			},
+			{
+				input:  []float64{1, 0},
+				result: 1,
+			},
+			{
+				input:  []float64{1, 1},
+				result: 1,
+			},
+		}
 
-	p.SetWeight([]float64{1, 1, 0.5})
-	p.SetBiasWeight(1)
-	out := p.Run([]float64{1, 1, 1})
-	require.EqualValues(t, 1, out)
+		p := New()
+		p.SetWeight([]float64{0, 0})
+		p.SetBiasWeight(0)
 
-	p.SetWeight([]float64{0, 0, 0})
-	p.SetBiasWeight(0.1)
-	out = p.Run([]float64{1, 1, 1})
-	require.EqualValues(t, 0, out)
+		matched := 0
+		for n, sample := range data {
+			result := p.Run(sample.input)
+			fmt.Printf("Test %d: actual %f expected %f\n", n, result, sample.result)
+			if result == sample.result {
+				matched++
+			}
+		}
+		fmt.Printf("Matched %d from %d\n", matched, len(data))
+
+	})
+	t.Run("logic", func(t *testing.T) {
+		p := New()
+
+		p.SetWeight([]float64{1, 1, 0.5})
+		p.SetBiasWeight(1)
+		out := p.Run([]float64{1, 1, 1})
+		require.EqualValues(t, 1, out)
+
+		p.SetWeight([]float64{0, 0, 0})
+		p.SetBiasWeight(0.1)
+		out = p.Run([]float64{1, 1, 1})
+		require.EqualValues(t, 0, out)
+	})
 }
