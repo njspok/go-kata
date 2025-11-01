79676c8101dc6cee023584be5f1359b1bd29c674
Khamov Oleg <njspok@gmail.com>
2025-11-01 18:41:23 +0500

    fractional

--- fractional/fractional_test.go
+++ fractional/fractional_test.go
@@ -8,74 +8,51 @@ import (
 
 // todo rewrite tests in style "2+3=5", "2/5+5/3=31/15"
 
-func Test(t *testing.T) {
+func Test2(t *testing.T) {
 	tests := []struct {
-		name      string
-		a         Fractional
-		b         Fractional
-		operation string
-		result    Fractional
+		name string
+		expr string
 	}{
 		{
-			name:      "sum zero and number",
-			a:         Number(0),
-			b:         Number(33),
-			operation: "plus",
-			result:    Number(33),
+			name: "sum with zero",
+			expr: "12+0=12",
 		},
 		{
-			name:      "sum two numbers",
-			a:         Number(11),
-			b:         Number(33),
-			operation: "plus",
-			result:    Number(44),
+			name: "sum two numbers",
+			expr: "11+22=33",
 		},
 		{
-			name:      "sum two fractions",
-			a:         Frac(2, 3),
-			b:         Frac(4, 5),
-			operation: "plus",
-			result:    Frac(22, 15),
+			name: "sum two fractions",
+			expr: "2|3+4|5=22|15",
 		},
 		{
-			name:      "sum fractions and number",
-			a:         Number(11),
-			b:         Frac(4, 5),
-			operation: "plus",
-			result:    Frac(59, 5),
+			name: "sum fraction and number",
+			expr: "11+4|5=59|5",
 		},
 		{
-			name:      "minus two numbers",
-			a:         Number(11),
-			b:         Number(22),
-			operation: "minus",
-			result:    Number(-11),
+			name: "minus two numbers",
+			expr: "11-22=-11",
 		},
 		{
-			name:      "minus two fractions",
-			a:         Frac(2, 3),
-			b:         Frac(4, 5),
-			operation: "minus",
-			result:    Frac(-2, 15),
+			name: "minus two fractions",
+			expr: "2|3-4|5=-2|15",
 		},
 		{
-			name:      "sum fractions and number",
-			a:         Number(11),
-			b:         Frac(4, 5),
-			operation: "minus",
-			result:    Frac(51, 5),
+			name: "sum fractions and number",
+			expr: "11-4|5=51|5",
 		},
 	}
-
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			switch test.operation {
-			case "plus":
-				require.Equal(t, test.result, test.a.Plus(test.b))
-			case "minus":
-				require.Equal(t, test.result, test.a.Minus(test.b))
+		t.Run(test.name+": "+test.expr, func(t *testing.T) {
+			a, b, c, operation, err := ParseExpression(test.expr)
+			require.NoError(t, err)
+			switch operation {
+			case "+":
+				require.Equal(t, c, a.Plus(b))
+			case "-":
+				require.Equal(t, c, a.Minus(b))
 			default:
-				require.Fail(t, "unexpected operation")
+				require.Fail(t, "unknown operation")
 			}
 		})
 	}
