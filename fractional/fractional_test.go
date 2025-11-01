5683ee93e39a8b9cec5c2a4ee3ec697e974ded78
Khamov Oleg <njspok@gmail.com>
2025-11-01 18:46:55 +0500

    fractional

--- fractional/fractional_test.go
+++ fractional/fractional_test.go
@@ -7,42 +7,18 @@ import (
 )
 
 func Test(t *testing.T) {
-	tests := []struct {
-		name string
-		expr string
-	}{
-		{
-			name: "sum with zero",
-			expr: "12+0=12",
-		},
-		{
-			name: "sum two numbers",
-			expr: "11+22=33",
-		},
-		{
-			name: "sum two fractions",
-			expr: "2|3+4|5=22|15",
-		},
-		{
-			name: "sum fraction and number",
-			expr: "11+4|5=59|5",
-		},
-		{
-			name: "minus two numbers",
-			expr: "11-22=-11",
-		},
-		{
-			name: "minus two fractions",
-			expr: "2|3-4|5=-2|15",
-		},
-		{
-			name: "sum fractions and number",
-			expr: "11-4|5=51|5",
-		},
+	tests := []string{
+		"12+0=12",
+		"11+22=33",
+		"2|3+4|5=22|15",
+		"11+4|5=59|5",
+		"11-22=-11",
+		"2|3-4|5=-2|15",
+		"11-4|5=51|5",
 	}
 	for _, test := range tests {
-		t.Run(test.name+": "+test.expr, func(t *testing.T) {
-			a, b, c, operation, err := ParseExpression(test.expr)
+		t.Run(test, func(t *testing.T) {
+			a, b, c, operation, err := ParseExpression(test)
 			require.NoError(t, err)
 			switch operation {
 			case "+":
