0b095925192a2c92ca27d203e69b6ff5e5257116
Khamov Oleg <njspok@gmail.com>
2025-11-01 18:52:32 +0500

    fractional

--- fractional/fractional.go
+++ fractional/fractional.go
@@ -1,19 +1,31 @@
 package fractional_calc
 
 import (
-	"regexp"
 	"strconv"
-
-	"github.com/pkg/errors"
 )
-
-var rx = regexp.MustCompile(`^(\d+)\|?(\d*)([+\-*/])(\d+)\|?(\d*)(=)(-?\d+)\|?(\d*)$`)
 
 func Frac(num, denum int) Fractional {
 	return Fractional{
 		num:   num,
 		denum: denum,
 	}
+}
+
+func FracFromString(num, denum string) (Fractional, error) {
+	n, err := strconv.Atoi(num)
+	if err != nil {
+		return Fractional{}, err
+	}
+
+	d := 1
+	if denum != "" {
+		d, err = strconv.Atoi(denum)
+		if err != nil {
+			return Fractional{}, err
+		}
+	}
+
+	return Frac(n, d), nil
 }
 
 func Number(i int) Fractional {
@@ -48,48 +60,4 @@ func (f Fractional) Minus(m Fractional) Fractional {
 
 func (f Fractional) Parts() (num, denum int) {
 	return f.num, f.denum
-}
-
-func FracFromString(num, denum string) (Fractional, error) {
-	n, err := strconv.Atoi(num)
-	if err != nil {
-		return Fractional{}, err
-	}
-
-	d := 1
-	if denum != "" {
-		d, err = strconv.Atoi(denum)
-		if err != nil {
-			return Fractional{}, err
-		}
-	}
-
-	return Frac(n, d), nil
-}
-
-func ParseExpression(str string) (a, b, c Fractional, operation string, err error) {
-	groups := rx.FindAllStringSubmatch(str, -1)
-
-	if len(groups) != 1 || len(groups[0]) != 9 {
-		return a, b, c, operation, errors.New("invalid expression")
-	}
-
-	a, err = FracFromString(groups[0][1], groups[0][2])
-	if err != nil {
-		return a, b, c, operation, err
-	}
-
-	operation = groups[0][3]
-
-	b, err = FracFromString(groups[0][4], groups[0][5])
-	if err != nil {
-		return a, b, c, operation, err
-	}
-
-	c, err = FracFromString(groups[0][7], groups[0][8])
-	if err != nil {
-		return a, b, c, operation, err
-	}
-
-	return a, b, c, operation, nil
 }
---
+++ fractional/parsers.go
@@ -0,0 +1,38 @@
+package fractional_calc
+
+import (
+	"regexp"
+
+	"github.com/pkg/errors"
+)
+
+// regex for pars strings operations in two fractions like
+// 2+2=4 or 2|5+1|3=11|15
+var rx = regexp.MustCompile(`^(\d+)\|?(\d*)([+\-*/])(\d+)\|?(\d*)(=)(-?\d+)\|?(\d*)$`)
+
+func ParseExpression(str string) (a, b, c Fractional, operation string, err error) {
+	groups := rx.FindAllStringSubmatch(str, -1)
+
+	if len(groups) != 1 || len(groups[0]) != 9 {
+		return a, b, c, operation, errors.New("invalid expression")
+	}
+
+	a, err = FracFromString(groups[0][1], groups[0][2])
+	if err != nil {
+		return a, b, c, operation, err
+	}
+
+	operation = groups[0][3]
+
+	b, err = FracFromString(groups[0][4], groups[0][5])
+	if err != nil {
+		return a, b, c, operation, err
+	}
+
+	c, err = FracFromString(groups[0][7], groups[0][8])
+	if err != nil {
+		return a, b, c, operation, err
+	}
+
+	return a, b, c, operation, nil
+}
