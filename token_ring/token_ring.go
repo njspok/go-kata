fd2cc86f445eae4dcb2fcd298fdf4481eab42c64
Khamov Oleg <njspok@gmail.com>
2026-06-27 12:09:58 +0500

    token ring

--- token_ring/token_ring.go
+++ token_ring/token_ring.go
@@ -5,3 +5,23 @@ package token_ring
 // они передают друг другу маркер
 // если процесс принял маркер, и ему он не нужен, то он передает его дальше
 // если процесс приянл маркер и он ему нужен, то он выполняет действие и передает маркер дальше
+
+func NewRing() *Ring {
+	r := &Ring{
+		ch: make(chan struct{}, 1),
+	}
+	r.ch <- struct{}{}
+	return r
+}
+
+type Ring struct {
+	ch chan struct{}
+}
+
+func (r *Ring) TakeToken() {
+	<-r.ch
+}
+
+func (r *Ring) PutToken() {
+	r.ch <- struct{}{}
+}
--- token_ring/token_ring_test.go
+++ token_ring/token_ring_test.go
@@ -7,26 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func NewRing() *Ring {
-	r := &Ring{
-		ch: make(chan struct{}, 1),
-	}
-	r.ch <- struct{}{}
-	return r
-}
-
-type Ring struct {
-	ch chan struct{}
-}
-
-func (r *Ring) TakeToken() {
-	<-r.ch
-}
-
-func (r *Ring) PutToken() {
-	r.ch <- struct{}{}
-}
-
 func Test(t *testing.T) {
 	ring := NewRing()
 
